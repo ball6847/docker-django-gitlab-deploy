9f61fb216e72e7e598c8a7c5df7da8841ec484f3
Porawit Poboonma <porawit.p@bizidea.co.th>
2013-11-22 11:22:07 +0700

    set allowed domain for production

--- settings/production.py
+++ settings/production.py
@@ -7,7 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 TIME_ZONE = 'Asia/Bangkok'
 
 # you need to change this on production environment
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ci.bizidea.co.th']
 
 DATABASES = {
 	'default': {
