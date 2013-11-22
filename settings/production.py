a20e88a62aa1256d3459a6a62382f4eb96ef0136
Porawit Poboonma <porawit.p@bizidea.co.th>
2013-11-22 09:42:42 +0700

    add ALLOWED_HOSTS to production.py

--- settings/production.py
+++ settings/production.py
@@ -1,17 +1,17 @@
-DEBUG = False
+from .base import PROJECT_ROOT
+
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 TIME_ZONE = 'Asia/Bangkok'
 
+# you need to change this on production environment
+ALLOWED_HOSTS = []
+
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': '',                      # Or path to database file if using sqlite3.
-		# The following settings are not used with sqlite3:
-		'USER': '',
-		'PASSWORD': '',
-		'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-		'PORT': '',                      # Set to empty string for default.
+		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': PROJECT_ROOT.child('db', 'database.sqlite'),                      # Or path to database file if using sqlite3.
 	}
 }
 
