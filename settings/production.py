e262a264bde17413aa0c59bfc9fe13bd41419802
Porawit Poboonma <porawit.p@bizidea.co.th>
2014-01-13 12:23:44 +0700

    add email configuration in production and add email testing apps

--- settings/production.py
+++ settings/production.py
@@ -1,10 +1,14 @@
-from .base import PROJECT_ROOT
+from .base import PROJECT_ROOT, ADMINS
 
 # change to True in development
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 TIME_ZONE = 'Asia/Bangkok'
+
+ADMIN += (
+    ('ball6847', 'porawit.p@bizidea.co.th'),
+)
 
 # you need to change this on production environment
 ALLOWED_HOSTS = ['.gitlab-deploy.bizidea.co.th']
@@ -18,3 +22,10 @@ DATABASES = {
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ktluyf=j=chx8+%dz*1c^8j%^ru3u5fs6-+z+07w+nz_&y5e!-'
+
+# email setting, need if you need django to send email to ADMIN when errors occured
+EMAIL_HOST = '127.0.0.1'
+EMAIL_HOST_USER = 'bizidea'
+EMAIL_HOST_PASSWORD = 'nyGJELRR'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
