8118560173db128cc6295a1552093756a9c0dae5
root <root@www.bizidea.co.th>
2014-02-10 11:25:15 +0700

    fix cronjob file to start service whenever its gone

--- bin/cronjob_monitor.sh
+++ bin/cronjob_monitor.sh
@@ -2,11 +2,12 @@
 
 # used in production server only
 
-PSCOUNT="$(ps aux | grep [g]itlab_deploy_server | wc -l)"
+PSCOUNT="$(ps aux | grep [g]itlab_deployd | wc -l)"
 
 if [ "$PSCOUNT" -eq "0" ]; then
+    echo "$(date +"%D %T")" " - starting gitlab_deploy_server"
     cd "$(dirname $0)/.."
     source .virtualenv/bin/activate
-    gunicorn wsgi:application --name gitlab_deploy_server --workers 3 --bind=unix:/var/run/gitlab_deploy_server.sock --daemon
+    gunicorn wsgi:application --name gitlab_deployd --workers 3 --bind=unix:/var/run/gitlab_deploy_server.sock --daemon
 fi
 
