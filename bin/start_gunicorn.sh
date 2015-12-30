230d95935a4beee74f97e42d2ddb1f62d3b55a3e
Porawit Poboonma <porawit.p@bizidea.co.th>
2015-12-30 17:13:15 +0700

    modify startup script

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -3,30 +3,23 @@
 cd "$(dirname $0)/.."
 
 NAME="gitlab_deploy_server"
-NUM_WORKERS=3
+BIND=127.0.0.1:54321
+NUM_WORKERS=1
 DJANGO_SETTINGS_MODULE="settings.base"
 DJANGO_WSGI_MODULE="wsgi"
 
 if [[ $VIRTUAL_ENV = "" ]]; then
-	VIRTUAL_ENV=".virtualenv"
+	VIRTUAL_ENV=".venv"
 	source "$VIRTUAL_ENV/bin/activate"
 fi
 
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$PWD:$PYTHONPATH
 
-SOCKFILE="/var/run/gitlab_deploy_server.sock"
-
-# Create the run directory if it doesn't exist
-RUNDIR="$(dirname $SOCKFILE)"
-test -d $RUNDIR || mkdir -p $RUNDIR
-
-echo "starting gunicorn using unix socket: $SOCKFILE"
- 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 exec "$VIRTUAL_ENV/bin/gunicorn" "$DJANGO_WSGI_MODULE:application" \
 	--name $NAME \
 	--workers $NUM_WORKERS \
 	--log-level=debug \
-	--bind=unix:$SOCKFILE
+	--bind=$BIND
