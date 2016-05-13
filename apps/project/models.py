fbc0d76cf97e2971f955a17af3efa357832a3712
Porawit Poboonma <ball6847@gmail.com>
2016-05-13 21:45:13 +0700

    fix model indent

--- apps/project/models.py
+++ apps/project/models.py
@@ -5,17 +5,13 @@ from django.forms.models import model_to_dict
 from tasks import deploy
 
 class Project(models.Model):
-	name = models.CharField(max_length=255, blank=False,
-                            help_text="Name of project (just for reminding purpose).")
-	repo = models.CharField(max_length=255, blank=False,
-                            help_text="SSH URL of the repository (eg. git@git.bizidea.co.th:Bizidea/bizidea-mailer.git)")
-    branch = models.CharField(max_length=255, blank=False, default="master"
-                            help_text="Source branch to pull from (default is master)")
-	path = models.CharField(max_length=255, blank=False,
-                            help_text="Absolute path on server to clone repo into.")
+    name = models.CharField(max_length=255, blank=False, help_text="Name of project (just for reminding purpose).")
+    repo = models.CharField(max_length=255, blank=False, help_text="SSH URL of the repository (eg. git@git.bizidea.co.th:Bizidea/bizidea-mailer.git)")
+    branch = models.CharField(max_length=255, blank=False, default="master", help_text="Source branch to pull from (default is master)")
+    path = models.CharField(max_length=255, blank=False, help_text="Absolute path on server to clone repo into.")
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 @receiver(post_save, sender=Project, dispatch_uid="notify_worker")
 def notify_worker(sender, instance, **kwargs):
